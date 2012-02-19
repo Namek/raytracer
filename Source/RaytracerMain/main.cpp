18fdb7421a0c86c77eb899cbf62137300d326045
Namek <kamil@namekdev.net>
2012-02-19 20:39:29 +0000

    * added: reading params.txt * moved project files

--- Source/RaytracerMain/main.cpp
+++ Source/RaytracerMain/main.cpp
@@ -9,11 +9,26 @@ using namespace Utils;
 using namespace nprt;
 
 
-int main(int, char* )
-{
+int main(int argc, char* argv[])
+{	
+	char params[1024];
+	std::string paramsFileName = "params.txt";
+	std::ifstream paramsFile(paramsFileName);
+
+	if (argc > 0)
+		paramsFileName = argv[0];
+
+	if (paramsFile.is_open())
+	{
+		memset(params, 0, sizeof(params));
+		paramsFile.getline(params, sizeof(params));
+		paramsFile.close();
+	}
+
+	// TODO Interpret parameters
+
 	Scene sc;
 	sc.LoadGeometry("data/room.brs");
-	
 
 	_getch();
 
