be7c822d72c42f2dd4c0dbfbc3e5758e76f0732e
Namek <kamil@namekdev.net>
2012-05-27 17:55:46 +0000

    wczytywanie mocy ("power") dla swiatel

--- Source/RaytracerMain/LightSource.cpp
+++ Source/RaytracerMain/LightSource.cpp
@@ -4,10 +4,11 @@
 using namespace nprt;
 
 
-LightSource::LightSource(float x, float y, float z, float r, float g, float b)
+LightSource::LightSource(float x, float y, float z, float r, float g, float b, float power)
 {
 	position = Vector3d(x, y, z);
 	this->r = r;
 	this->g = g;
 	this->b = b;
+	this->power = power;
 }
--- Source/RaytracerMain/LightSource.h
+++ Source/RaytracerMain/LightSource.h
@@ -9,9 +9,10 @@ namespace nprt
 	{	
 		public:
 			LightSource() {	}
-			LightSource(float x, float y, float z, float r, float g, float b);
+			LightSource(float x, float y, float z, float r, float g, float b, float power);
 
 			Vector3d position;
 			float r, g, b;
+			float power;
 	};
 }
